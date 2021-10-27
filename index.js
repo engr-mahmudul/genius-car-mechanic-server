bb8a04ef9a0d484e20300d721248159c2b7f6b1e
Mahmudul Hasan <17101120@uap-bd.edu>
2021-10-27 16:02:28 +0600

    data add,delete and show complete

--- index.js
+++ index.js
@@ -45,6 +45,13 @@ async function run() {
 
             res.json(result);
         });
+        //Delete API
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
 
 
     }
